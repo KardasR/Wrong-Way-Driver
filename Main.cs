d046a08e978fcc05541273dfb14128d3c6294e96
KardasR <38445317+KardasR@users.noreply.github.com>
2025-01-27 15:29:52 -0500

    add main scene

--- Main.cs
+++ Main.cs
@@ -1,28 +1,27 @@
 using Godot;
 using System;
 
-public partial class Mob : RigidBody2D
+public partial class Main : Node
 {
 
 	#region Member Variables
 
-	private AnimatedSprite2D animatedSprite2D;
-
+	private int _score;
+	
 	#endregion
 
 	#region Signals
 
-	[Signal]
-	public delegate void ScreenExitedEventHandler();
+	#endregion
+
+	#region Exports
+
+	[Export]
+	public PackedScene MobScene { get; set; }
 
 	#endregion
 
 	#region Constructors
-
-	public Mob()
-	{
-		
-	}
 
 	#endregion
 
@@ -31,27 +30,16 @@ public partial class Mob : RigidBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// setup the animated sprite and start playing it.
-		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		animatedSprite2D.Animation = "drive";
-
-		animatedSprite2D.Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
 	}
 
 	#endregion
 
 	#region Methods
-
-	private void OnVisibleOnScreenNotifier2DScreenExited()
-	{
-		QueueFree();
-	}
 
 	#endregion
 }
