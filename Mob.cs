2b5dd24bb6456e581557a397d9750190079a7726
KardasR <38445317+KardasR@users.noreply.github.com>
2025-01-27 15:17:17 -0500

    added some code to the wrong way drivers

--- Mob.cs
+++ Mob.cs
@@ -3,13 +3,55 @@ using System;
 
 public partial class Mob : RigidBody2D
 {
+
+	#region Member Variables
+
+	private AnimatedSprite2D animatedSprite2D;
+
+	#endregion
+
+	#region Signals
+
+	[Signal]
+	private delegate void ScreenExitedEventHandler();
+
+	#endregion
+
+	#region Constructors
+
+	public Mob()
+	{
+		
+	}
+
+	#endregion
+
+	#region Override Methods
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// setup the animated sprite and start playing it.
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Animation = "drive";
+
+		animatedSprite2D.Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
+
+	#endregion
+
+	#region Methods
+
+	private void OnVisibleOnScreenNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
+
+	#endregion
 }
